17109f8772a3e4a70423f0bcaad079371e13f0f9
dweinmann <dontspamme@gmx.org>
2020-09-14 22:04:42 +0200

    Refactor Calculation of change and inserted money

--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
@@ -119,14 +120,16 @@ namespace Fahrkartenautomat.ViewModel
                 message.AppendLine();
                 message.AppendLine($"Wechselgeld: {change:0.00}€");
                 message.AppendLine();
-                message.AppendLine(CalculateChange());
+
+                message.AppendLine(GetStringFromMoneyList(CalculateChange()));
+                //message.AppendLine(CalculateChange());
 
                 MessageBox.Show(message.ToString(), "Nett Geschäfte mit dir zu machen :)", MessageBoxButton.OK, MessageBoxImage.Information);
 
                 EnteredMoneyBits.Clear();
                 ShoppingCart.ToList().ForEach(ticket => ticket.Amount = 0);
                 ShoppingCart.Clear();
-                RefillShoppinCart();
+                RefillShoppingCart();
                 RaisePropertyChanged("TotalPrice");
                 RaisePropertyChanged("EnteredMoney");
             }
@@ -139,9 +142,10 @@ namespace Fahrkartenautomat.ViewModel
         private void EjectMoney()
         {
             StringBuilder message = new StringBuilder();
-            message.AppendLine($"Geld wird zurückgegeben: {EnteredMoney:0.00}€");
+            message.AppendLine($"Eingezahltes Geld wird zurückgegeben: {EnteredMoney:0.00}€");
             message.AppendLine();
-            EnteredMoneyBits.ForEach(m => message.AppendLine($"1x " + (m >= 1 ? $"{m} €" : $"{(m * 100):0} Cent")));
+            message.AppendLine(GetStringFromMoneyList(EnteredMoneyBits));
+
             MessageBox.Show(message.ToString(), "Geld wird ausgegeben :)", MessageBoxButton.OK, MessageBoxImage.Information);
 
             EnteredMoneyBits.Clear();
@@ -169,7 +173,7 @@ namespace Fahrkartenautomat.ViewModel
             {
                 found.Amount++;
             }
-            RefillShoppinCart();
+            RefillShoppingCart();
         }
 
         public bool ItemIsSelected(object param) 
@@ -186,10 +190,12 @@ namespace Fahrkartenautomat.ViewModel
             {
                 ShoppingCart.Remove(ticket);
             }
-            RefillShoppinCart();
+            RefillShoppingCart();
         }
 
-        private void RefillShoppinCart()
+        // Needed to trigger Changed event on ObservableCollection. 
+        // Changing a property of an element in the list is not enough.
+        private void RefillShoppingCart()
         {
             var tmpList = new ObservableCollection<Ticket>();
 
@@ -209,7 +215,6 @@ namespace Fahrkartenautomat.ViewModel
             RaisePropertyChanged("TotalPrice");
         }
 
-
         private Ticket IsInShoppingCart(Ticket ticket)
         {
             var found = ShoppingCart.Where(t => t.Type == ticket.Type);
@@ -220,26 +225,34 @@ namespace Fahrkartenautomat.ViewModel
             return null;
         }
 
-        private string CalculateChange()
+        private List<decimal> CalculateChange()
         {
-            StringBuilder sb = new StringBuilder();
-            var total = EnteredMoney - TotalPrice;
+            List<decimal> moneyList = new List<decimal>();
 
+            var total = EnteredMoney - TotalPrice;
             PossibleMoney.OrderByDescending(x=>x).ToList().ForEach(m =>
             {
-                int amount = 0;
                 while (total >= m)
                 {
-                    amount++;
                     total -= m;
-                }
-                if (amount > 0)
-                {
-                    sb.AppendLine($"{amount}x " + (m >= 1 ? $"{m} €" : $"{(m * 100):0} Cent"));
+                    moneyList.Add(m);
                 }
             });
+            return moneyList;
+        }
+
+        private string GetStringFromMoneyList(List<decimal> moneyList)
+        {
+            StringBuilder sb = new StringBuilder();
+            moneyList.GroupBy(m => m)
+                     .ToList()
+                     .ForEach(m =>
+                         sb.AppendLine($"{m.Count()}x " + (m.Key >= 1 ? $"{m.Key} €" : $"{(m.Key * 100):0} Cent"))
+                     );
+
             return sb.ToString();
         }
+
 
         private void RaisePropertyChanged([CallerMemberName] string propertyName = "")
         {
