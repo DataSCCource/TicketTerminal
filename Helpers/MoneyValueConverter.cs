c22d0a3de8c00b89054a117f7d812e4920c88f66
DennisW <dontspamme@gmx.org>
2020-09-19 11:17:42 +0200

    Add MoneyValueConverter for converting money values
    * Values below 1 > e.g. 0.1 -> 10 ct
    * Values from 1 -> 2 -> 2 €

---
+++ Helpers/MoneyValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Fahrkartenautomat
+{
+    public class MoneyValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var amount = (decimal)value;
+
+            if(amount < 1)
+            {
+                amount *= 100;
+                return $"{(int)amount} ct";
+            }
+            return $"{amount} €";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
